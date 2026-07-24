5b06f5a630ac67225c4dd2e2e1a7a93295b6ad20
brian772 <ratatito2@gmail.com>
2026-07-24 10:57:16 +0700

    Add PageController methods and article view template

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,11 +9,15 @@ class PageController extends Controller
   public function index() {
       return 'Hi! Selamat Datang di Website Laravel';
   }
+
     public function about() {
-        return view('about', ['title' => 'about', 'nis' => '25978', 'nama' => 'Brian Ardhisswara', 'kelas' => 'XI RPA']);
+        return view('about', ['title' => 'About Page', 'nis' => '25978', 'nama' => 'Brian Ardhisswara', 'kelas' => 'XI RPA']);
     }
 
+    // public function article($id) {
+    //     return 'Halaman Artikel ke-' . $id;
+    // }
     public function article($id) {
-        return 'Halaman Artikel ke-' . $id;
+        return view('article', ['id' => $id]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return 'Hi! Selamat Datang di Website Laravel';
+// });
+// Route::get('/index', function () {
+//     return view('index', ['title' => 'Index Page']);
+// });
+// Route::get('/about', function () {
+//     return view('about', ['title' => 'About Page', 'nis' => '25978', 'nama' => 'Brian Ardhisswara', 'kelas' => 'XI RPA']);
+// });
+// Route::get('/article/{id}', function ($id) {
+//     return 'Halaman Artikel ke-' . $id;
+// });
+
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/index', [PageController::class, 'index']);
+Route::get('/article/{id}', [PageController::class, 'article']);
